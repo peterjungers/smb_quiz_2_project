b5aca5fa18322783fea530550d16228c3ddb29a1
Peter Jungers <peter.jungers@pcc.edu>
2024-03-27 09:36:47 -0700

    Initial commit

---
+++ smb_quiz_2/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from smb_quiz_2.config import Config
+
+# initialized outside of app so that they aren't bound to specific app
+# (using Blueprints allows me to create multiples instances of app,
+# but I can have same db, users, etc., across all instances)
+db = SQLAlchemy()
+# bcrypt
+# login_manager
+# mail
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+
+    from smb_quiz_2.errors.handlers import errors
+    from smb_quiz_2.main.routes import main
+    from smb_quiz_2.users.routes import users
+    app.register_blueprint(errors)
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+
+    return app
---
+++ smb_quiz_2/main/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('users', __name__)
