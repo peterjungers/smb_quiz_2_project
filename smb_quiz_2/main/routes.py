dab2341b861ef2c9aa4130b43f92409f91298213
Peter Jungers <peter.jungers@pcc.edu>
2024-04-04 11:59:30 -0700

    Refactoring routes.py, first pass through

--- smb_quiz_2/main/routes.py
+++ smb_quiz_2/main/routes.py
@@ -16,104 +16,100 @@ def index():
     return render_template("index.html")
 
 
-@main.route("/quiz")  # create one route for each level instead
-def quiz():
-    # new query
-    query = db.session.execute(
-        select(Question)
-        .filter(Question.level == 1)
-    ).all()
-    # print(query)
-    random.shuffle(query)
-    print(query)
-    # for s in query:
-    #     print(s.Question)
+# If route is manually typed, sent back to index (make it go to level 1?, but can't make work):
+@main.route("/quiz")
+def testing():
+    return render_template("index.html")
 
-    # for q in query:
-    #     print(q.Question.answer)
 
-    query_five = query[0:5]
-    print(query_five)
+@main.route("/quiz/<level_num>")
+def quiz(level_num):
+    """ """
+    def get_questions(level_num):
+        questions = db.session.execute(
+                    select(Question)
+                    .filter(Question.level == level_num)
+                    ).all()
+        random.shuffle(questions)
+        questions = questions[0:5]
 
-    # q_ids = []
-    # for q in query:
-    #     q_id = q.Question.id
-    #     q_ids.append(q_id)
-    #     # print(q_id)
-    #
-    # # Establish list of unique numeric values:
-    # values = []
-    # while len(values) < 5:
-    #     v = random.randrange(min(q_ids), max(q_ids))
-    #     if v not in values:
-    #         values.append(v)
-    #         # print(v)
+        return questions
 
-    # Query database using those unique values as primary key value:
-    # questions = []
-    # for v in values:
-    #     # Query returns object consisting of a
-    #     # question, correct answer, two wrong answers:
-    #     q = db.session.execute(
-    #         select(Question)
-    #         .filter(Question.id == v)
-    #         )
-    #     questions.append(q)
+    def get_elements(quiz_questions):
+        correct_answers = []
+        for q in quiz_questions:
+            correct = q.Question.answer
+            correct_answers.append(correct)
 
-    # Separate elements of each Question object so answers can be shuffled:
-    num = 1
-    quiz = []
-    correct_answers = []
-    for q in query_five:  # For each question object in list:
-        # for row in q:  # For each question:
-        correct = q.Question.answer
-        correct_answers.append(correct)
-        answer_options = [
-            q.Question.answer,
-            q.Question.wrong_answer_1,
-            q.Question.wrong_answer_2
-        ]
-        random.shuffle(answer_options)
-        # Question number, question, question's three answer options:
-        num_question_options = (num,
-                                q.Question.question,
-                                answer_options)
-        quiz.append(num_question_options)
-        num += 1
+        quiz = []
+        num = 1
+        for q in quiz_questions:
+            answer_options = [
+                q.Question.answer,
+                q.Question.wrong_answer_1,
+                q.Question.wrong_answer_2
+            ]
+            random.shuffle(answer_options)
 
-    # Encryption so correct answers aren't readily visible in dev tools:
-    def random_characters(integer):
-        # Help from https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
-        random_string = "".join(random.choices(
-            string.ascii_uppercase
-            + string.ascii_lowercase
-            + string.digits
-            + string.punctuation,
-            k=integer)
-        )
-        return random_string
+            num_question_options = (num,
+                                    q.Question.question,
+                                    answer_options)
+            quiz.append(num_question_options)
 
-    coded_answers = []
-    for answer in correct_answers:
-        coded_answer = ""
-        for letter in answer:
-            letter = chr(ord(letter) + 5)
-            coded_answer += letter
-        key = str(len(coded_answer))
-        # This is so I know all keys are two digits:
-        if int(key) < 10:
-            key = "0" + key
-        coded_answer = coded_answer + key
+            num += 1
 
-        while len(coded_answer) < 200:
-            random_char = random_characters(1)
-            coded_answer = random_char + coded_answer
+        return correct_answers, quiz
 
-        coded_answer = coded_answer + random_characters(200)
-        coded_answers.append(coded_answer)
+    def encrypt_answers(correct_answers):
+        """ """
+        def random_characters(amount):
+            # Help from https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
+            random_string = "".join(random.choices(
+                            string.ascii_uppercase
+                            + string.ascii_lowercase
+                            + string.digits
+                            + string.punctuation,
+                            k=amount)
+                            )
+            return random_string
 
-        # score = 100
+        def create_coded_answers(correct_answers):
+            coded_answers = []
+            for answer in correct_answers:
+                coded_answer = ""
+                for letter in answer:
+                    letter = chr(ord(letter) + 5)
+                    coded_answer += letter
+
+                key = str(len(coded_answer))
+                # This is so I know all keys are two digits:
+                if int(key) < 10:
+                    two_digit_key = "0" + key
+                else:
+                    two_digit_key = key
+                coded_answer = coded_answer + two_digit_key
+
+                # Make all coded answers have length of 200 characters:
+                while len(coded_answer) < 200:
+                    random_char = random_characters(1)
+                    coded_answer = random_char + coded_answer
+
+                # Append 200 more random characters at end of coded answer:
+                coded_answer = coded_answer + random_characters(200)
+                coded_answers.append(coded_answer)
+
+            return coded_answers
+
+        coded_answers = create_coded_answers(correct_answers)
+        return coded_answers
+
+    quiz_questions = get_questions(level_num)
+    correct_answers, quiz = get_elements(quiz_questions)
+    coded_answers = encrypt_answers(correct_answers)
+
+    # score = 100
 
     return render_template("quiz.html",
                            quiz=quiz,
+                           level_num=level_num,
                            array=coded_answers)
